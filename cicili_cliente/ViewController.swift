af39f7d7be515e5d2b8723019f86486383fbfa0c
agsneutron <ags_neutron@hotmail.com>
2019-12-17 01:25:09 -0600

    Initial Commit

---
+++ cicili_cliente/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cicili_cliente
+//
+//  Created by ARIANA SANCHEZ on 17/12/19.
+//  Copyright © 2019 CICILI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
