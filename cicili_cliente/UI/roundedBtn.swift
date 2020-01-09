d5be27dda5a06a493c112e5852633119df8e2a6c
guillermosanchez <nexus.gsan@gmail.com>
2020-01-09 01:57:34 -0600

    up

---
+++ cicili_cliente/UI/roundedBtn.swift
@@ -0,0 +1,32 @@
+//
+//  roundedBtn.swift
+//  cicili_cliente
+//
+//  Created by guillermo sanchez on 07/01/20.
+//  Copyright © 2020 CICILI. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundButton: UIButton {
+
+    @IBInspectable var cornerRadius: CGFloat = 0{
+        didSet{
+        self.layer.cornerRadius = cornerRadius
+        }
+        
+    }
+
+    @IBInspectable var borderWidth: CGFloat = 0{
+        didSet{
+            self.layer.borderWidth = borderWidth
+        }
+    }
+
+    @IBInspectable var borderColor: UIColor = UIColor.clear{
+        didSet{
+            self.layer.borderColor = borderColor.cgColor
+        }
+    }
+}
