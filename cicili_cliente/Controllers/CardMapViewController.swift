3b498b121858459be38fdbd6a1464795fdf43273
ariaocho <ariaocho@gmail.com>
2020-01-14 09:45:45 -0600

    merge

---
+++ cicili_cliente/Controllers/CardMapViewController.swift
@@ -0,0 +1,13 @@
+//
+//  CardMapViewController.swift
+//  cicili_cliente
+//
+//  Created by ARIANA SANCHEZ on 08/01/20.
+//  Copyright © 2020 CICILI. All rights reserved.
+//
+
+import UIKit
+
+class CardMapViewController: UIViewController {
+    @IBOutlet weak var handleArea: UIView!
+}
---
+++ cicili_cliente/Storyboards/TestSwiftUIView.swift
@@ -0,0 +1,21 @@
+//
+//  TestSwiftUIView.swift
+//  cicili_cliente
+//
+//  Created by ARIANA SANCHEZ on 06/01/20.
+//  Copyright © 2020 CICILI. All rights reserved.
+//
+
+import SwiftUI
+
+struct TestSwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TestSwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        TestSwiftUIView()
+    }
+}
