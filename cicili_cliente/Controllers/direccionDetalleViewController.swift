0e6e3a1044f237765884455e7733b720ea00be1d
guillermosanchez <nexus.gsan@gmail.com>
2020-01-27 22:08:45 -0600

    ok

---
+++ cicili_cliente/Controllers/direccionDetalleViewController.swift
@@ -0,0 +1,15 @@
+//
+//  direccionDetalleViewController.swift
+//  cicili_cliente
+//
+//  Created by guillermo sanchez on 27/01/20.
+//  Copyright © 2020 CICILI. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class direccionDetalleViewController: UIViewController {
+    
+    
+}
