587f351701fe5dfe86c237db7154b5d8e15efed9
agsneutron <ags_neutron@hotmail.com>
2020-01-30 16:10:46 -0600

    card view reloaded

--- cicili_cliente/Controllers/CardMapViewController.swift
+++ cicili_cliente/Controllers/CardMapViewController.swift
@@ -8,65 +8,19 @@
 
 import UIKit
 
-class CardMapViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class CardMapViewController: UIViewController{
     @IBOutlet weak var handleArea: UIView!
     
     
-    @IBOutlet weak var tblAddressView: UITableView!
-    var addressArray = [AddressTable]()
-    
+    @IBOutlet weak var txtAlgo: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        guard let address1 = AddressTable(id: 1, name: "Casa") else {
-            fatalError("Unable to instantiate meal1")
-        }
-
-        guard let address2 = AddressTable(id: 2, name: "Trabajo") else {
-            fatalError("Unable to instantiate meal2")
-        }
-
-        guard let address3 = AddressTable(id: 3, name: "Casa papás") else {
-            fatalError("Unable to instantiate meal2")
-        }
         
-        addressArray += [address1, address2, address3]
-        
-        tblAddressView.dataSource = self
-        tblAddressView.delegate = self
 
     }
     
     
-    //MARK:- UITableView methods
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return addressArray.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        var cell = tableView.dequeueReusableCell(withIdentifier: "productstable")
-        if cell == nil {
-            cell = UITableViewCell(style: .subtitle, reuseIdentifier: "productstable")
-        }
-        
-        cell?.textLabel?.text = addressArray[indexPath.row].name
-        return cell!
-    }
-
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        //performSegue(withIdentifier: "presentHome", sender: self)
-        
-    }
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if let destination = segue.destination as? MainViewController {
-            destination.addresRequest = addressArray[(tblAddressView.indexPathForSelectedRow?.row)!]
-            tblAddressView.deselectRow(at: tblAddressView.indexPathForSelectedRow!, animated: true)
-
-        }
-    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
