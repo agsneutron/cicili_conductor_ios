2d9a75a992ca9fdea68bad4164b205d1808cb6c8
guillermosanchez <nexus.gsan@gmail.com>
2020-02-01 10:15:12 -0600

    ok

--- cicili_cliente/Controllers/PedidoHistoricoTableViewCell.swift
+++ cicili_cliente/Controllers/PedidoHistoricoTableViewCell.swift
@@ -19,10 +19,17 @@ class PedidoHistoricoTableViewCell: UITableViewCell {
     @IBOutlet weak var litrosLabel: UILabel!
     @IBOutlet weak var precioLabel: UILabel!
     
-   
+    @IBOutlet weak var ContentCardView: UIView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        ContentCardView.layer.cornerRadius = 20.0
+          ContentCardView.layer.shadowColor = UIColor.gray.cgColor
+          ContentCardView.layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+          ContentCardView.layer.shadowRadius = 9.0
+          ContentCardView.layer.shadowOpacity = 0.6
+        
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
