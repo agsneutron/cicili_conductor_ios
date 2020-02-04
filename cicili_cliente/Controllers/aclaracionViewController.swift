10a872b9c996ceab8149f11169d6620bb245ba73
guillermosanchez <nexus.gsan@gmail.com>
2020-02-03 21:48:47 -0600

    ok

---
+++ cicili_cliente/Controllers/aclaracionViewController.swift
@@ -0,0 +1,22 @@
+//
+//  aclaracionViewController.swift
+//  cicili_cliente
+//
+//  Created by guillermo sanchez on 01/02/20.
+//  Copyright © 2020 CICILI. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class aclaracionViewController: UIViewController {
+    
+    @IBAction func closeButton(_ sender: Any) {
+         dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+    super.viewDidLoad()
+      
+    }
+}
