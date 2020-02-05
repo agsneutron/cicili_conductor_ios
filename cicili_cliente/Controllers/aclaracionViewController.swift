79593567426f1fb8ca08e5901d8d43397aa774ca
guillermosanchez <nexus.gsan@gmail.com>
2020-02-04 19:56:33 -0600

    ok

--- cicili_cliente/Controllers/aclaracionViewController.swift
+++ cicili_cliente/Controllers/aclaracionViewController.swift
@@ -8,8 +8,11 @@
 
 import Foundation
 import UIKit
+var selectedCategory: String?
+var categoryList = ["Pedidos", "Seguridad", "Uso de la App", "Pagos", "Regitro", "Mis preguntas"]
 
-class aclaracionViewController: UIViewController {
+class aclaracionViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, UITextFieldDelegate {
+    @IBOutlet weak var aclaracionText: UITextField!
     
     @IBAction func closeButton(_ sender: Any) {
          dismiss(animated: true, completion: nil)
@@ -17,6 +20,39 @@ class aclaracionViewController: UIViewController {
     
     override func viewDidLoad() {
     super.viewDidLoad()
-      
+      createPickerView()
+           dismissPickerView()
+
     }
+
+    func createPickerView() {
+           let pickerView = UIPickerView()
+           pickerView.delegate = self
+           aclaracionText.inputView = pickerView
+    }
+    func dismissPickerView() {
+       let toolBar = UIToolbar()
+       toolBar.sizeToFit()
+       let button = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(self.action))
+       toolBar.setItems([button], animated: true)
+       toolBar.isUserInteractionEnabled = true
+       aclaracionText.inputAccessoryView = toolBar
+    }
+    @objc func action() {
+          view.endEditing(true)
+    }
+    
+        func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1 // number of session
+        }
+        func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return categoryList.count // number of dropdown items
+        }
+        func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return categoryList[row] // dropdown item
+        }
+        func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        selectedCategory = categoryList[row] // selected item
+        aclaracionText.text = selectedCategory
+        }
 }
